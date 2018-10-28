93cd98d79ed0b9a4e92faf9e95b3f5b9130991d0
Robert Xu <robertyc.xu@gmail.com>
2018-10-27 22:02:55 -0700

    linked tmux.conf

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-# installz zsh configs
+# install zsh configs
 rm ~/.zshrc
-ln -s ./zsh/zshrc ~/.zshrc
+ln -s ./dotfiles/zsh/.zshrc ~/.zshrc
+
+# install oh my zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # install powerline fonts
 # clone
@@ -20,6 +23,7 @@ git clone https://github.com/RobertYCXu/vim-config .vim
 # link .vimrc with vimrc in .vim
 ln -s ~/.vim/vimrc ~/.vimrc
 
-# install oh my zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# install tmux configs
+rm ~/.tmux.conf
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
